a5dbfb9b9e12190acd2f4cba7f19933404c40fe9
petrstovicek1@gmail.com <petrstovicek1@gmail.com>
2022-04-16 12:36:36 +0200

    Readme

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 import setuptools
-import pathlib
-import pkg_resources
-
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
 
 setuptools.setup(
     name='ecc_project',
@@ -18,5 +9,5 @@ setuptools.setup(
     author='Petr Šťovíček',
     author_email='petrstovicek1@gmail.com',
     description='App for ECDH, ECDSA, ECIES, P2P',
-    install_requires=install_requires
+    install_requires=['pygubu', 'pycryptodome']
 )
